01edd9efdc7830760d6cbc9bb4c7da59116361e9
李佩诺 <297787003@qq.com>
2019-12-03 23:37:48 +0800

    树的深度 树的镜像

---
+++ TreeDepth/Solution.java
@@ -0,0 +1,40 @@
+package TreeDepth;
+
+public class Solution {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+    //输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+    public int TreeDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        int leftDepth = TreeDepth(root.left);
+        int rightDepth = TreeDepth(root.right);
+        return 1 + (leftDepth > rightDepth ? leftDepth:rightDepth);
+    }
+
+
+//操作给定的二叉树，将其变换为源二叉树的镜像。
+    public void Mirror(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+        Mirror(root.right);
+        return;
+    }
+}
