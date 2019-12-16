479bd754f2832ac49e82b712e2ceefb14567a654
李佩诺 <297787003@qq.com>
2019-12-16 23:54:30 +0800

    数组中出现次数超过一半的数字

---
+++ MoreThanHalfNum/Solution.java
@@ -0,0 +1,22 @@
+package MoreThanHalfNum;
+
+import java.util.*;
+public class Solution {
+    public int MoreThanHalfNum_Solution(int [] array) {
+        int len=array.length;
+        if(len<1){
+            return 0;
+        }
+        int count=0;
+        Arrays.sort(array);
+        int num=array[len/2];
+        for(int i=0;i<len;i++){
+            if(num==array[i])
+                count++;
+        }
+        if(count<=(len/2)){
+            num=0;
+        }
+        return num;
+    }
+}
