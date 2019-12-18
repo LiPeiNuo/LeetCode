50be5ae934c3864b920b9658eab4c371c302f4ca
李佩诺 <297787003@qq.com>
2019-12-18 22:28:49 +0800

    连续子数组的最大和

---
+++ FindGreatestSumOfSubArray/Solution.java
@@ -0,0 +1,21 @@
+package FindGreatestSumOfSubArray;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<array.length;i++){
+            int sum = 0;
+            for(int j=i;j<array.length;j++){
+                sum += array[j];
+                list.add(sum);
+            }
+        }
+        if(list.size() <=0) return 0;
+        Collections.sort(list);
+        return list.get(list.size()-1);
+    }
+}
