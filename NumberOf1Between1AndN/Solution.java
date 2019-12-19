692da465a212c7de11c09bb4e15ea6a18195ce5f
李佩诺 <297787003@qq.com>
2019-12-19 23:52:17 +0800

    整数中1出现的次数

---
+++ NumberOf1Between1AndN/Solution.java
@@ -0,0 +1,17 @@
+package NumberOf1Between1AndN;
+
+public class Solution {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int count=0;
+        while(n>0){
+            String str=String.valueOf(n);
+            char [] chars=str.toCharArray();
+            for(int i=0;i<chars.length;i++){
+                if(chars[i]=='1')
+                    count++;
+            }
+            n--;
+        }
+        return count;
+    }
+}
