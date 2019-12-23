cefde5508fcbc9fa13ac634d689305e5f7f5d6a0
李佩诺 <297787003@qq.com>
2019-12-23 23:54:14 +0800

    栈的压入、弹出序列

---
+++ IsPopOrder/Solution.java
@@ -0,0 +1,22 @@
+package IsPopOrder;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        if(pushA.length == 0 || popA.length == 0){
+            return false;
+        }
+        Stack <Integer> s = new Stack<Integer>();
+        int popIndex = 0;
+        for(int i = 0; i < pushA.length; i++){
+            s.push(pushA[i]);
+            while(!s.empty() && s.peek() == popA[popIndex]){
+                s.pop();
+                popIndex++;
+            }
+        }
+        return s.empty();
+    }
+}
