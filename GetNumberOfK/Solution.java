c5125c4111ba21fc290dcd34b165945be5dff159
李佩诺 <297787003@qq.com>
2019-12-26 23:25:48 +0800

    数字在排序数组中出现的次数

---
+++ GetNumberOfK/Solution.java
@@ -0,0 +1,13 @@
+package GetNumberOfK;
+
+public class Solution {
+    public int GetNumberOfK(int [] array , int k) {
+        int count = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == k){
+                count++;
+            }
+        }
+        return count;
+    }
+}
