702742e70403734f102bcaa0097c0290fd1dde16
李佩诺 <297787003@qq.com>
2019-12-28 23:42:46 +0800

    左旋转字符串

---
+++ LeftRotateString/Solution.java
@@ -0,0 +1,16 @@
+package LeftRotateString;
+
+public class Solution {
+    public String LeftRotateString(String str,int n) {
+        if(str.length() == 0){
+            return str;
+        }
+        StringBuffer buffer = new StringBuffer(str);
+        StringBuffer buffer1 = new StringBuffer(str);
+        StringBuffer buffer2 = new StringBuffer();
+        buffer.delete(0,n);
+        buffer1.delete(n,str.length());
+        buffer2.append(buffer.toString()).append(buffer1.toString());
+        return buffer2.toString();
+    }
+}
