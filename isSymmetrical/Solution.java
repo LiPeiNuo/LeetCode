e9a45a5c3a4c30d28e8263fc6608f7d98a0d6890
李佩诺 <297787003@qq.com>
2020-01-07 23:47:03 +0800

    对称二叉树

---
+++ isSymmetrical/Solution.java
@@ -0,0 +1,39 @@
+package isSymmetrical;
+
+
+public class Solution {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    boolean isSymmetrical(TreeNode pRoot){
+        TreeNode node = getMirror(pRoot);
+        return isSymmetrical(pRoot,node);
+    }
+    boolean isSymmetrical(TreeNode pRoot,TreeNode node){
+        if(pRoot == null && node == null){
+            return true;
+        }else if(pRoot == null || node  == null){
+            return false;
+        }
+        if(pRoot.val == node.val){
+            return isSymmetrical(pRoot.left,node.left)&&isSymmetrical(pRoot.right,node.right);
+        }
+        return false;
+    }
+
+    TreeNode getMirror(TreeNode pRoot){
+        if (pRoot == null){
+            return null;
+        }
+        TreeNode root = new TreeNode(pRoot.val);
+        root.right = getMirror(pRoot.left);
+        root.left = getMirror(pRoot.right);
+        return root;
+    }
+}
+
