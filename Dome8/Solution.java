1cfeda8930d3eb2237a5f51b9bafe34f4b4f51ed
李佩诺 <297787003@qq.com>
2020-01-17 23:29:10 +0800

    判断第一个字符串能不能由第二个字符串里面的字符构成。

---
+++ Dome8/Solution.java
@@ -0,0 +1,15 @@
+package Dome8;
+
+public class Solution {
+    public boolean canConstruct(String ransom, String magazine) {
+        if (magazine.length() < ransom.length()) return false;
+        int caps[] = new int[26];
+        for (char c : ransom.toCharArray()) {
+            int index = magazine.indexOf(c, caps[c - 'a']);
+            if (index == -1)
+                return false;
+            caps[c - 97] = index + 1;
+        }
+        return true;
+    }
+}
