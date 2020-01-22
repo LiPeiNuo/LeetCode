dcb33cf4ca779019dea6b029366212ed90a7ac78
李佩诺 <297787003@qq.com>
2020-01-22 23:40:37 +0800

    寻找数组的中心索引

---
+++ Dome13/Solution.java
@@ -0,0 +1,15 @@
+package Dome13;
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0 , leftsum = 0;
+        for(int x : nums) sum+=x;
+        for(int i = 0; i < nums.length; ++i){
+            if(leftsum == sum - leftsum - nums[i]){
+                return i;
+            }
+            leftsum = leftsum + nums[i];
+        }
+        return -1;
+    }
+}
