64e4cc245479037cf8b1205924d4137165261c42
李佩诺 <297787003@qq.com>
2020-02-05 23:44:34 +0800

    子集

---
+++ Dome27/Solution.java
@@ -0,0 +1,41 @@
+package Dome27;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        int size = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (size == 0) {
+            return res;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < size + 1; i++) {
+            dfs(nums, 0, i, stack, res);
+        }
+        return res;
+    }
+
+    private void dfs(int[] nums, int start, int depth, Stack<Integer> path, List<List<Integer>> res) {
+        if (depth == path.size()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            path.add(nums[i]);
+            dfs(nums, i + 1, depth, path, res);
+            path.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        Solution solution = new Solution();
+        List<List<Integer>> subsets = solution.subsets(nums);
+        System.out.println(subsets);
+    }
+}
+
