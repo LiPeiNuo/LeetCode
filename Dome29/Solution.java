c8f4f31772502fda9881819b58104ad40a18214e
李佩诺 <297787003@qq.com>
2020-02-07 12:37:22 +0800

    序列化二叉树

---
+++ Dome29/Solution.java
@@ -0,0 +1,55 @@
+package Dome29;
+
+public class Solution {
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    String Serialize(TreeNode root) {
+        if(root == null)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        Serialize2(root, sb);
+        return sb.toString();
+    }
+
+    void Serialize2(TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            sb.append("#,");
+            return;
+        }
+        sb.append(root.val);
+        sb.append(',');
+        Serialize2(root.left, sb);
+        Serialize2(root.right, sb);
+    }
+
+    int index = -1;
+
+    TreeNode Deserialize(String str) {
+        if(str.length() == 0)
+            return null;
+        String[] strs = str.split(",");
+        return Deserialize2(strs);
+    }
+
+    TreeNode Deserialize2(String[] strs) {
+        index++;
+        if(!strs[index].equals("#")) {
+            TreeNode root = new TreeNode(0);
+            root.val = Integer.parseInt(strs[index]);
+            root.left = Deserialize2(strs);
+            root.right = Deserialize2(strs);
+            return root;
+        }
+        return null;
+    }
+
+}
+
