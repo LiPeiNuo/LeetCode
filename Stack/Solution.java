f3d175276ef32ceaa9404e1d2dba0de1ed652c4c
LiXiangNan <297787003@qq.com>
2020-02-20 23:22:34 +0800

    用两个栈实现队列

---
+++ Stack/Solution.java
@@ -0,0 +1,23 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int first=stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return first;
+    }
+}
