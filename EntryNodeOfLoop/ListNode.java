41091189ac9d3f16b955c7e34d1dd8590229d950
LiXiangNan <297787003@qq.com>
2020-04-08 11:43:36 +0800

    rename

---
+++ EntryNodeOfLoop/ListNode.java
@@ -0,0 +1,10 @@
+package EntryNodeOfLoop;
+
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ ReverseList/ListNode.java
@@ -0,0 +1,9 @@
+package ReverseList;
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
