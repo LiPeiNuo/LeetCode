36d094fd7eebcae8d0b46af9c41ff37d360646df
LiXiangNan <297787003@qq.com>
2020-05-11 13:11:31 +0800

    最大子序和

---
+++ maxSubArray/Solution.java
@@ -0,0 +1,35 @@
+package maxSubArray;
+
+/*给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+示例:
+
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-subarray
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int num:nums){
+            if(sum > 0){
+                sum += num;
+            }else{
+                sum = num;
+            }
+            ans = Math.max(sum,ans);
+        }
+        return ans;
+
+    }
+}
--- searchInsert/Solution.java
+++ searchInsert/Solution.java
@@ -1,18 +1,43 @@
-package searchInsert;
+package SearchInsert;
 
-class Solution {
+/*给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+
+输入: [1,3,5,6], 0
+输出: 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-insert-position
+*/
+public class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while(left <= right) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left < right){
             int mid = (left + right) / 2;
-            if(nums[mid] == target) {
+            if(target > nums[mid]){
+                left = mid +1 ;
+            }if(target < nums[mid]){
+                right = mid -1 ;
+            }if(target == nums[mid]){
                 return mid;
-            } else if(nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
             }
         }
-        return left;
+        return target <= nums[left] ? left : left + 1;
     }
 }
