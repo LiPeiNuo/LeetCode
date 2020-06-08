2254675df2bdd80d53d90cb415ea62d1d39c5302
LiXiangNan <297787003@qq.com>
2020-06-08 23:42:09 +0800

    两个数组的交集

---
+++ intersect/Solution.java
@@ -0,0 +1,46 @@
+package intersect;
+
+import java.util.Arrays;
+import java.util.HashMap;
+/*
+350.两个数组的交集
+* 给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2,2]
+示例 2:
+
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [4,9]
+说明：
+
+输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+我们可以不考虑输出结果的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+*/
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> m = new HashMap<>();
+        for (int n : nums1) {
+            m.put(n, m.getOrDefault(n, 0) + 1);
+        }
+        int k = 0;
+        for (int n : nums2) {
+            int cnt = m.getOrDefault(n, 0);
+            if (cnt > 0) {
+                nums1[k++] = n;
+                m.put(n, cnt - 1);
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+
+}
