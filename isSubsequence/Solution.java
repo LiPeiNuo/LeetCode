f3b429e2cf6d8f5f8e29493df97984ed987f8a9a
LiXiangNan <297787003@qq.com>
2020-06-15 23:08:12 +0800

    判断子序列

---
+++ isSubsequence/Solution.java
@@ -0,0 +1,37 @@
+package isSubsequence;
+/*
+392.判断子序列
+* 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+
+字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+示例 1:
+s = "abc", t = "ahbgdc"
+
+返回 true.
+
+示例 2:
+s = "axc", t = "ahbgdc"
+
+返回 false.
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/is-subsequence
+*/
+
+public class Solution {
+    public boolean isSubsequence(String s, String t) {
+        char[] arr = s.toCharArray();
+        int j = -1;
+        for(int i = 0;i<arr.length;i++) {
+            j = t.indexOf(arr[i],j+1);
+            if(j==-1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
